d50df39484c75178389151eb87b5211d90365b2c
SimoneShaw911 <1992businessasusual@gmail.com>
2022-12-15 04:30:26 -0500

    got help

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -152,22 +152,20 @@ class TestAccountService(TestCase):
 
 
     def test_update_account(self):
-        """ should create, update, and verify updated account"""
-
+        """It should Update an existing Account"""
+        # create an Account to update
         test_account = AccountFactory()
-        resp = self.client.post(
-            BASE_URL,
-            json=test_account.serialize()
-        )
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
         self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
 
+        # update the account
         new_account = resp.get_json()
-        new_account["name"] = "something known"
+        new_account["name"] = "Something Known"
         resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-
         updated_account = resp.get_json()
-        self.assertEqual(updated_account["name"], "something known")
+        self.assertEqual(updated_account["name"], "Something Known")
+
 
     # ADD YOUR TEST CASES HERE ...
 
